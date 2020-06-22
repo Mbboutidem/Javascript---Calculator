4d0859c4821aaf28c7318b60d66a718021332599
Mb <mboutidemarchibong@yahoo.com>
2020-06-22 14:20:50 +0200

    fix the bug in calculator, if the user click the dot multiple times,

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement)
     {
-        this.currentOperandTextElement = currentOperandTextElement
-        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.clearValues();
     }
         
     
@@ -16,25 +17,88 @@ class Calculator {
 
     delet()
     {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
 
     }
-    appendNumber(number)
+    appendNumber(number) {
+        const currentOperand = '';
+        //condition to check for periods
+        if (number === '.' && this.currentOperand.includes('.')) return
+         this.currentOperand = this.currentOperand.toString() + number.toString();
+      }
+    chooseOperation(operation)
     {
-    
-        this.currentOperand =  this.currentOperand.toString() + number.toString();
-
-    }
-    chooseOperation(operator)
-    {
+        if (this.currentOperand === '') return
+        if (this.previousOperand !== ''){
+            this.compute();
+        }
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
 
     }
     compute()
     {
+        let computation;
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+          //
+          if(isNaN(prev) || isNaN(current)) return;
+          //
+          switch(this.operation)
+          {
+              case '+':
+                  computation = prev + current;
+                  break;
+              case '-':
+                  computation = prev - current;
+                  break;
+              case '*':
+                  computation = prev * current;
+                  break;
+              case '/':
+                  computation = prev / current;
+                  break;
+              default:
+                  return;
+          }
+          this.currentOperand = computation;
+          this.operation = undefined;
+          this.previousOperand = '';
+          this.readytoReset = true;
 
+    }
+    getDisplay(number)
+    {
+        const stringNumber = number.toString();
+        const integerNumber = parseFloat(stringNumber.split('.')[0]);
+        const decimalValues = stringNumber.split('.')[1];
+        let integerDisplay;
+        if(isNaN(integerNumber))
+        {
+            integerDisplay = '';
+
+        }else {
+            integerDisplay =  integerNumber.toLocaleString('en', {
+                maximumFractionDigits: 0
+            })
+        }
+        if (decimalValues != null){
+            return `${integerDisplay}.${decimalValues}`
+        }else {
+            return integerNumber;
+        }
     }
     updateDisplay()
     {
-        this.currentOperandTextElement.innerText = this.currentOperand;
+        this.currentOperandTextElement.innerText = this.getDisplay(this.currentOperand);
+        if(this.operation != null)
+        {
+            this.previousOperandTextElement.innerText = `${this.getDisplay(this.previousOperand)} ${this.operation}`
+        } else {
+            this.previousOperandTextElement.innerText = '';
+
+        }
 
     }
 }
@@ -50,7 +114,38 @@ const calculator = new Calculator(previousOperandTextElement, currentOperandText
 
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
+        if(calculator.previousOperand === "" && calculator.currentOperand !== "" &&
+        calculator.readytoReset){
+            calculator.currentOperand = "";
+            calculator.readytoReset = false;
+        }
         calculator.appendNumber(button.innerText);
         calculator.updateDisplay();
     })
+})
+
+//operation buttons
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText);
+        calculator.updateDisplay(); //update the display
+    })
+})
+equalsButton.addEventListener('click', button => {
+    calculator.compute();
+    calculator.updateDisplay();
+
+})
+//clear button
+allClearButton.addEventListener('click', button => {
+    calculator.clearValues();
+    calculator.updateDisplay();
+
+})
+
+//delet  button
+deleteButton.addEventListener('click', button => {
+    calculator.delet();
+    calculator.updateDisplay();
+
 })
